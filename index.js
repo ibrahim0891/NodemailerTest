81c2517694af599fcee4e3e27b8d9d7d80e00622
ibrahim0891 <nijhum0891@gmail.com>
2024-12-15 10:22:36 +0600

    sk

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+const nodemailer = require('nodemailer');
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'nijhum0891@gmail.com',
+        pass: 'wnir oxqy iiuq qtjd'
+    }
+})
+
+
+const sendEmail = async (to, subject, text) => {
+    try {
+        const mailOptions = {
+            from: 'nijhum0891@gmail.com',
+            to,
+            subject,
+            text
+        }
+        const info = await transporter.sendMail(mailOptions)
+        console.log('Email sent: ', info.response);
+    }
+    catch (err) {
+        console.log("Error sendng Email ", err)
+    };
+}
+
+sendEmail('dayofad642@lineacr.com', 'Hello, You have due books', '324253')
